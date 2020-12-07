01a892e5222a39dc598ad749733f7b1c8fd46c9d
José Augusto <70649235+joseaorufino@users.noreply.github.com>
2020-12-06 22:39:00 -0300

    Update main.py
    Now, the game has a Menu and ask the player if he will play again.

--- ChocoRocket/main.py
+++ ChocoRocket/main.py
@@ -1,48 +1,17 @@
-# choco_rocket_beta.py
-
 import pygame
 import sys
 import os
 import random
 import time
 from pygame.locals import *
+from model import Enemies, Player, Chocolate
+
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 def load_img(name, w, h):
    path = os.path.join(main_dir,"images",name)
    return pygame.transform.scale(pygame.image.load(path), (w,h))
-
-class Enemies:
-    
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = load_img('meteor.png', 80, 55)
-
-    def draw(self, window):
-        window.blit(self.image, (self.x, self.y))
-
-class Player:
-    
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = load_img('p1.png', 70, 80)
-
-    def draw(self, window):
-        window.blit(self.image, (self.x, self.y))
-
-class Chocolate:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = load_img('choco.png', 50, 50)
-
-    def draw(self, window):
-        window.blit(self.image, (self.x, self.y))
-
 
 def main():
     pygame.init()
@@ -57,22 +26,73 @@ def main():
     BG = load_img("bg.png", WIDTH, HEIGHT)
 
     clock = pygame.time.Clock()
-    lives = 3
-    score = 0
+    
     font = pygame.font.SysFont("comicsans", 40)
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
-    game_over = font.render("Game Over!", True, WHITE)
+    RED = (255, 0, 0)
 
     x, y = 0, 200 
-    player = Player(x, y)
-    meteor = Enemies(660, random.randint(15, 345))
-    choco = Chocolate(660, random.randint(15, 345))
-    vel = 4.5
-    vel_meteor = 4.5
-    vel_choco = 4
+    player = Player(x, y, load_img('p1.png', 70, 80))
+    meteor = Enemies(660, random.randint(15, 345), load_img('meteor.png', 80, 55))
+    choco = Chocolate(660, random.randint(15, 345), load_img('choco.png', 50, 50))
 
-    def redraw_window():
+    def button(msg,x,y,w,h,action=None):
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        font_small = pygame.font.SysFont('courier', 35)
+            
+        pygame.draw.rect(WIN, BLACK, (x, y, w, h))
+
+        if x+w > mouse[0] > x and y+h > mouse[1] > y:
+            message = font_small.render(f"> {msg} <", True, WHITE)
+            WIN.blit(message, (x, y))
+            
+            if click[0] == 1 and action != None:
+                if action == 'play':
+                    game_loop() 
+
+                elif action == 'quit':
+                    pygame.quit()
+                    quit()
+
+                elif action == 'play again':
+                    player.x, player.y = 0, 200
+                    meteor.x, meteor.y = 660, random.randint(15, 345)
+                    choco.x, choco.y = 660, random.randint(15, 345)
+                    game_loop()
+
+        
+        else:
+            start = font_small.render(f"< {msg} >", True, WHITE)
+            WIN.blit(start, (x, y))
+
+    def game_intro():
+
+        intro = True
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            WIN.fill(BLACK)
+            font_intro = pygame.font.SysFont("courier", 60)
+            title = font_intro.render("ChocoRocket", True, RED)
+            WIN.blit(title, (165, 0))
+
+            button("Start", 250, 150, 200, 40,'play')
+            button("Credits", 250, 210, 200, 40,'credits')
+            button("Quit", 250, 270, 200, 40,'quit')
+
+            pygame.display.update()
+            clock.tick(15)
+
+
+    def redraw_window(lives, score):
         WIN.blit(BG, (0, 0))
         # label score and lives on the screen
         lives_label = font.render(f"Lives: {lives}", 1, (255, 255, 255))
@@ -102,68 +122,84 @@ def main():
 
         return False
 
-    # Game loop
-    while True:
-        clock.tick(60)
-        redraw_window()
+    def game_loop():
+        lives = 3
+        score = 0
+        game_over = font.render("Game Over!", True, WHITE)
 
-        score_label = font.render(f"Score: {score}", 1, (255, 255, 255))
-        
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+        vel = 4.5
+        vel_meteor = 4.5
+        vel_choco = 4
 
-        keys = pygame.key.get_pressed()
+        while True:
+            clock.tick(60)
+            redraw_window(lives, score)
 
-        if keys[pygame.K_UP] and player.y - vel > 20:
-            player.y -= vel
-        if keys[pygame.K_DOWN] and player.y + vel + 80 < HEIGHT:
-            player.y += vel
-        
-        meteor.x -= vel_meteor
-        choco.x -= vel_choco
-
-        # Meteor
-        if meteor.x == player.x or meteor.x < player.x:
+            score_label = font.render(f"Score: {score}", 1, (255, 255, 255))
             
-            # check for collision
-            if colision(meteor):
-                lives -= 1
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-            meteor.y = random.randint(32, 400-55)
-            meteor.x = 660
-            vel_meteor +=  0.30
-            if vel_meteor > 14:
-                vel_meteor = 14
+            keys = pygame.key.get_pressed()
 
-        # Chocolate
-        if choco.x == player.x or choco.x < player.x:
-
-            #check for collision
-            if colision(choco):
-                score += 1
-
-            choco.y = random.randint(32, 400-50)
-            choco.x = 660
-            vel_choco += 0.25
-
-            if vel_choco > 12:
-                vel_choco = 12
-
-        if lives == 0:
-            time.sleep(1)
-
-            WIN.fill(BLACK)
-            WIN.blit(game_over, (260,180))
-            WIN.blit(score_label, (260,140))
-            pygame.display.update()
-
-            time.sleep(2)
+            if keys[pygame.K_UP] and player.y - vel > 20:
+                player.y -= vel
+            if keys[pygame.K_DOWN] and player.y + vel + 80 < HEIGHT:
+                player.y += vel
             
-            pygame.quit()
-            sys.exit()
+            meteor.x -= vel_meteor
+            choco.x -= vel_choco
 
+            # Meteor
+            if meteor.x == player.x or meteor.x < player.x:
+                
+                # check for collision
+                if colision(meteor):
+                    lives -= 1
+
+                meteor.y = random.randint(32, 400-55)
+                meteor.x = 660
+                vel_meteor +=  0.30
+                if vel_meteor > 14:
+                    vel_meteor = 14
+
+            # Chocolate
+            if choco.x == player.x or choco.x < player.x:
+
+                #check for collision
+                if colision(choco):
+                    score += 1
+
+                choco.y = random.randint(32, 400-50)
+                choco.x = 660
+                vel_choco += 0.25
+
+                if vel_choco > 12:
+                    vel_choco = 12
+
+            if lives == 0:
+                time.sleep(1)
+                
+                final = True
+                
+                while final:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            quit()
+                    
+                    WIN.fill(BLACK)
+                    WIN.blit(game_over, (260,180))
+                    WIN.blit(score_label, (260,140))
+                    button("Play Again", 50, 350, 350, 40,'play again')
+                    button("Quit", 450, 350, 200, 40,'quit')
+
+                    pygame.display.update()
+
+    
+    game_intro()
 
 if __name__ == "__main__":
     main()
