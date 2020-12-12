0cb86d1dd26898a00c6e87e9b81bc500e0ad5b37
José Augusto <70649235+joseaorufino@users.noreply.github.com>
2020-12-11 22:49:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="choco-rocket",
-    version="0.1.2",
+    version="0.2.0",
     author="Jose Augusto O. Rufino",
     author_email="joseaorufino@gmail.com",
     description="Game's type is Arcade.",
